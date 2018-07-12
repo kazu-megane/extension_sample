2759cb50f4480e2a59d511f05afd9e42f402bab3
kazu-megane <kazukazu.music1214@gmail.com>
2018-07-13 01:05:41 +0900

    ちょっと見るボタンの数を表示

---
+++ README.md
@@ -0,0 +1,2 @@
+# extension_sample
+[find()で子要素を取得する手法まとめ！](https://www.sejuku.net/blog/37474)
---
+++ script.js
@@ -0,0 +1,9 @@
+// console.log($('.elQuickViewDescription').find('.elQuickViewStock'));
+var sum = 0;
+
+$('.elQuickViewToggle').each(function(index, element){
+  // console.log(index+element.textContent);
+  sum++;
+})
+
+console.log(sum);
